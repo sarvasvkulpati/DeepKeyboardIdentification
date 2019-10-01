dae5ab81a113b5d28b7deff5cdf207df85fdfb0a
Sarvasv Kulpati <sarvasvkulpati@gmail.com>
2019-10-01 13:06:57 +0800

    fixed None bug on recording for space, return and shift

--- test.py
+++ test.py
@@ -23,14 +23,11 @@ def access_square_center(x, y):
 
     return return_x, return_y
 
-
 #input the high level grid of letters, get the underlying pixel coords
 def access_square(x, y):
     processedx = (x*2) - 1
     processedy = (y*2) - 1
     return access_square_center(processedx, processedy)
-
-
 
 def overlay_circle(image, x, y, size, color = 'red'):
     overlay = Image.new('RGBA', (image.size))
@@ -43,8 +40,6 @@ def overlay_line(image, startx, starty, endx, endy, width, color='red'):
     overlay_draw = ImageDraw.Draw(overlay)  # Create a context for drawing things on it.
     overlay_draw.line((startx, starty, endx, endy), fill=color, width=width)
     return Image.alpha_composite(image, overlay)
-
-
 
 def duration_to_color(max_duration, min_duration, duration, color_range, transparency):
 
@@ -62,16 +57,12 @@ def duration_to_color(max_duration, min_duration, duration, color_range, transpa
     return_color = tuple([int(x* 255) for x in return_color])
     return (*return_color, transparency)
 
-
 def duration_to_size(max_duration, min_duration, duration, size_range):
     OldRange = (max_duration - min_duration)  
     print(OldRange)
     size = int((duration - min_duration) * size_range / OldRange) +5 
     return size
 
-
-
-# print(recorded)
 def convert_to_draw_instructions(recorded):
 
     cache = []
@@ -83,10 +74,11 @@ def convert_to_draw_instructions(recorded):
         # if the current letter doesn't exist in the cache, add it to the cache
         if not any(event['name'] == currentEvent.name for event in cache):
             cache.append({'name': currentEvent.name, 'num' : 1, 'start' : currentEvent.time, 'end':None})
-
+            print("adding " + currentEvent.name + " to the cache")
         # if it does exist in the cache
         else:
-            relevantEvents = [event for event in cache if event['name'] is currentEvent.name]
+            relevantEvents = [event for event in cache if event['name'] == currentEvent.name]
+            print("relevantEvents for " + currentEvent.name + " are " + str(relevantEvents))
             try:
                 if relevantEvents[len(relevantEvents) - 1]['num'] is 2:
                     #if the last one was a complete press, add new event to cache
@@ -96,6 +88,8 @@ def convert_to_draw_instructions(recorded):
                     relevantEvents[len(relevantEvents) - 1].update({'num' : 2, 'end': currentEvent.time})
             except IndexError:
                 print('indexerror for: ' + currentEvent.name)
+                print(cache)
+                print(str(currentEvent.name == 'space'))
                 cache.append({'name': currentEvent.name, 'num' : 1, 'start' : currentEvent.time, 'end':None})
 
     cacheWithLines = []
@@ -125,9 +119,6 @@ def convert_to_draw_instructions(recorded):
     min_duration_circles = min(circle_timings)
 
     return cacheWithLines, min_duration_lines, max_duration_lines, min_duration_circles, max_duration_circles
-
-
-
 
 def draw_from_instructions(image, instructions, min_duration_lines, max_duration_lines, min_duration_circles, max_duration_circles):
 
@@ -168,10 +159,7 @@ def draw_from_instructions(image, instructions, min_duration_lines, max_duration
     image.save('img.png')
     
 instructions, min_duration_lines, max_duration_lines, min_duration_circles, max_duration_circles = convert_to_draw_instructions(recorded)
-
 draw_from_instructions(image, instructions, min_duration_lines, max_duration_lines, min_duration_circles, max_duration_circles)
-
-
 
 keyboard.wait()   
 
@@ -198,5 +186,5 @@ Maybe keep program running in background and continously record snippet of text.
 - Will need to stop recording if the user pauses typing- v. long gaps will skew data. 
 - Maybe a 1 second pause means terminate process.
 
-TODO:Need to figure out why some events stay at None. It happens for spaces, shift and delete. Maybe press time is too fast?
+
 '''
